5e183ee0d0fa95b13dc75b2c9e981374baa13db7
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2023-09-05 20:31:06 -0700

    pause menu
    episode 21

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,41 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject uI;
+
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause ()
+    {
+        uI.SetActive(!uI.activeSelf);
+
+        if(uI.activeSelf)
+        {
+            Time.timeScale = 0f;
+            //Time.fixedDeltaTime when changing timescale to something other than 1 or 0
+        } else {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry ()
+    {
+        TogglePause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu ()
+    {
+        Debug.Log("Go to Menu");
+    }
+}
