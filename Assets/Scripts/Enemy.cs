e9ae8efdbef1b2edf4207482e541b291b0333dc6
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2023-09-06 22:50:34 -0700

    health bars
    episode 23

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,23 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public float startSpeed = 10f;
+    public float startHealth = 100f;
+
     [HideInInspector]
     public float speed;
-    public float health = 100f;
+    [HideInInspector]
+    private float health = 100f;
+
     public int worth = 25;
 
     public GameObject deathEffect;
 
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
     void Start ()
     {
         speed = startSpeed;
+        health = startHealth;
     }
 
     public void TakeDamage (float amount)
     {
         health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
 
         if (health <= 0)
         {
