0aa233c5049bea0897726ac01c3fa86768341900
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2023-09-06 21:35:47 -0700

    main menu
    episode 22

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad = "MainScene";
+
+    public void Play ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void Quit ()
+    {
+        Debug.Log("Exiting");
+        Application.Quit();
+    }
+}
