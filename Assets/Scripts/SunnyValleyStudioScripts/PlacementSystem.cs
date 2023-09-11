c4f7db7e6f53a8cd014da926ae3f1505a3a47d25
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2023-09-10 22:26:36 -0700

    grid placement system in unity p 4 & 5
    placement collision detection, placement preview

--- Assets/Scripts/SunnyValleyStudioScripts/PlacementSystem.cs
+++ Assets/Scripts/SunnyValleyStudioScripts/PlacementSystem.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
@@ -8,8 +9,6 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     //gameobject that follows cursor
     private GameObject mouseIndicator;
-    [SerializeField]
-    private GameObject cellIndicator;
 
     [SerializeField]
     private InputManager inputmanager;
@@ -24,9 +23,24 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     private GameObject gridVisualization;
 
+    //placed flat tile
+    private GridData enemyTileData; //can place tile under object but not on other tiles
+    //placed objects on tile
+    private GridData playerTileData; //can place objects on tile but not on other objects
+
+    private List<GameObject> placedGameobjects = new();
+
+    [SerializeField]
+    private PreviewSystem preview;
+
+    private Vector3Int lastDetectedPosition = Vector3Int.zero;
+
     private void Start ()
     {
         StopPlacement();
+
+        enemyTileData = new();
+        playerTileData = new();
     }
 
     public void StartPlacement(int ID)
@@ -41,7 +55,7 @@ public class PlacementSystem : MonoBehaviour
             return;
         }
         gridVisualization.SetActive(true);
-        cellIndicator.SetActive(true);
+        preview.StartShowingPlacementPreview(database.objectsData[selectedObjectIndex].Prefab, database.objectsData[selectedObjectIndex].Size);
         inputmanager.OnClicked += PlaceStructure;
         inputmanager.OnExit += StopPlacement;
         Debug.Log("clicked");
@@ -58,19 +72,45 @@ public class PlacementSystem : MonoBehaviour
         Vector3 mousePosition = inputmanager.GetSelectedMapPosition();
         //converts world position of mouse to cell position
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+        if(placementValidity == false)
+        {
+            return;
+        }
+
         //creates prefab connected to object ID in database
         GameObject selectedObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab);
         //converts cell of grid position to world position, allows object to stay while mouse moves around inside cell due to int vector
         selectedObject.transform.position = grid.CellToWorld(gridPosition);
+
+        placedGameobjects.Add(selectedObject);
+
+        GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ? enemyTileData : playerTileData;
+
+        selectedData.AddObjectAt(gridPosition, database.objectsData[selectedObjectIndex].Size, database.objectsData[selectedObjectIndex].ID, placedGameobjects.Count - 1);
+
+        preview.UpdatePosition(grid.CellToWorld(gridPosition), false);
+    }
+
+    private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
+    {
+        //checks if the cell has an enemy tile
+        GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ? enemyTileData : playerTileData;
+
+        return selectedData.CanPlaceObjectAt(gridPosition, database.objectsData[selectedObjectIndex].Size);
     }
 
     private void StopPlacement()
     {
         selectedObjectIndex = -1;
         gridVisualization.SetActive(false);
-        cellIndicator.SetActive(false);
+        preview.StopShowingPreview();
+
         inputmanager.OnClicked -= PlaceStructure;
         inputmanager.OnExit -= StopPlacement;
+
+        lastDetectedPosition = Vector3Int.zero;
     }
 
     private void Update ()
@@ -83,9 +123,17 @@ public class PlacementSystem : MonoBehaviour
         Vector3 mousePosition = inputmanager.GetSelectedMapPosition();
         //converts world position of mouse to cell position
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
-        //mouseIndicator follows mouse inside cell, while cellIndicator stays until moved into new cell
-        mouseIndicator.transform.position = mousePosition;
-        //converts cell of grid position to world position, allows object to stay while mouse moves around inside cell due to int vector
-        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+
+        if(lastDetectedPosition != gridPosition)
+        {
+            bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+
+            //mouseIndicator follows mouse inside cell, while cellIndicator stays until moved into new cell
+            mouseIndicator.transform.position = mousePosition;
+
+            preview.UpdatePosition(grid.CellToWorld(gridPosition), placementValidity);
+
+            lastDetectedPosition = gridPosition;
+        }
     }
 }
