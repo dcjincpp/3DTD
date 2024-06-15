db01ef1243583d79273f9e27c232341b8a4bd873
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2024-06-14 17:25:36 -0700

    tower script

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    protected float cost;
+    protected float damage;
+    protected float attackSpeed;
+}
